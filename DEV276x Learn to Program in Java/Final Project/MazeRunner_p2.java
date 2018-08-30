ef3eceb80304501e0b9f78a4cdd374137282f881
daydreamersjp <42426597+daydreamersjp@users.noreply.github.com>
2018-08-30 14:26:56 -0400

    Create MazeRunner_p2.java

---
+++ DEV276x Learn to Program in Java/Final Project/MazeRunner_p2.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+public class MazeRunner_p2 {
+  
+  public static void main(String[] args) {
+    Maze myMap = new Maze();
+    intro();
+    user_move(myMap);
+    
+  }
+
+  public static void intro() {
+    System.out.println("Welcom to Maze Runner!");
+    System.out.println("Here is your current position:");
+    myMap.printMap();
+  }
+
+  public static void user_move(Maze myMap) {
+    String input = new Scanner();
+    
+    System.out.print("Where would you like to move? (R, L, U, D) ");
+    String move = input.next();
+    if (move != "R" && move != "L" && move != "U" && move != "D") {
+      System.out.println("Oops, wrong input. Try again!");
+      user_move();
+    } else if ( (move == "R" && myMap.canIMoveRight() == false) || (move == "L" && myMap.canIMoveLeft() == false) || (move == "U" && myMap.canIMoveUp() == false) || (move == "D" && myMap.canIMoveDown() == false) ){
+      System.out.println("Sorry, you’ve hit a wall.");
+      user_move();      
+    } else if (move == "R") {
+      myMap.moveRight();
+      myMap.printMap();
+    } else if (move == "L") {
+      myMap.moveLeft();
+      myMap.printMap();
+    } else if (move == "U") {
+      myMap.moveUp();
+      myMap.printMap();
+    } else {
+      myMap.moveDown();
+      myMap.printMap();
+    }
+    
+    if (myMap.didIWin() == true) {
+      System.out.println("Congratulations, you made it out alive!");
+    } else {
+      user_move();
+    }
+  }
+
+}
