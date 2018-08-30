da1292535056dad5ec58ddcddc243b83fb1bb18e
daydreamersjp <42426597+daydreamersjp@users.noreply.github.com>
2018-08-30 14:41:28 -0400

    Update MazeRunner_p2.java

--- DEV276x Learn to Program in Java/Final Project/MazeRunner_p2.java
+++ DEV276x Learn to Program in Java/Final Project/MazeRunner_p2.java
@@ -3,8 +3,9 @@ public class MazeRunner_p2 {
   
   public static void main(String[] args) {
     Maze myMap = new Maze();
+    int moves = 0;
     intro();
-    user_move(myMap);
+    user_move(myMap, move);
     
   }
 
@@ -14,7 +15,7 @@ public class MazeRunner_p2 {
     myMap.printMap();
   }
 
-  public static void user_move(Maze myMap) {
+  public static void user_move(Maze myMap, int moves) {
     String input = new Scanner();
     
     System.out.print("Where would you like to move? (R, L, U, D) ");
@@ -41,9 +42,22 @@ public class MazeRunner_p2 {
     
     if (myMap.didIWin() == true) {
       System.out.println("Congratulations, you made it out alive!");
+      System.out.println("and you did it in " + moves + " moves");
     } else {
       user_move();
     }
   }
+  
+  public static String movesMessage(int moves) {
+    if (moves == 50) {
+      System.out.println("Warning: You have made 50 moves, you have 50 remaining before the maze exit closes");
+    } else if (moves == 75) {
+      System.out.println("Alert! You have made 75 moves, you only have 25 moves left to escape.");
+    } else if (moves == 90) {
+      System.out.println("DANGER! You have made 90 moves, you only have 10 moves left to escape!!");
+    } else if (moves == 100) {
+      System.out.println("Oh no! You took too long to escape, and now the maze exit is closed FOREVER >:[");
+    }       
+  }  
 
 }
