150bf26ab8979423f993e56f89470bb4386dbf36
daydreamersjp <42426597+daydreamersjp@users.noreply.github.com>
2018-08-29 23:27:44 -0400

    Update Crypto.java

--- DEV276x Learn to Program in Java/Module 3/Crypto.java
+++ DEV276x Learn to Program in Java/Module 3/Crypto.java
@@ -7,8 +7,12 @@ public static class Crypto {
   
   // Part 1 - Normalize Text
   public static String normalizeText(String str) {
-  
-  
+    str = str.replaceAll(" ", "");
+    for (int i=0; i < ".,:;'\"!?()".length(), i++) {
+      str = str.replaceAll(".,:;'\"!?()".charAt(i), "");
+    }
+    str = str.toUpperCase();
+    return str;
   }
   
 
