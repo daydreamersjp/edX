64be74a094e8aa6a11409966237b6d0993d3ed97
daydreamersjp <42426597+daydreamersjp@users.noreply.github.com>
2018-08-30 10:03:04 -0400

    Update MazeRunner.java

--- DEV276x Learn to Program in Java/Final Project/MazeRunner.java
+++ DEV276x Learn to Program in Java/Final Project/MazeRunner.java
@@ -1,13 +1,32 @@
+import java.util.Scanner;
 public class MazeRunner {
   
   Maze myMap = new Maze();
   
   public static void main(String[] args) {
-    
+    intro();
+    user_move();
     
   }
 
+  public static void intro() {
+    System.out.println("Welcom to Maze Runner!");
+    System.out.println("Here is your current position:");
+    myMap.printMap();
+  }
 
+  public static String user_move() {
+    String input = new Scanner();
+    System.out.print("Where would you like to move? (R, L, U, D) ");
+    if (input.next() != "R" || input.next() != "L" || input.next() != "U" || input.next() != "D") {
+      System.out.println("Oops, wrong input. Try again!");
+      user_move();
+    } else {
+      return input.next();
+    }
+  }
+  
+  public static String 
 
 
 }
