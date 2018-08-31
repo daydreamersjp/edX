0fad5d979aa5c2a0abacb4160b49af62b8026e3a
daydreamersjp <42426597+daydreamersjp@users.noreply.github.com>
2018-08-30 21:47:39 -0400

    Update MazeRunner_part1.java

--- DEV276x Learn to Program in Java/Final Project/MazeRunner_part1.java
+++ DEV276x Learn to Program in Java/Final Project/MazeRunner_part1.java
@@ -1,51 +1,51 @@
 import java.util.Scanner;
-public class MazeRunner_part1 {
-  
-  Maze myMap = new Maze();
-  
-  public static void main(String[] args) {
-    
-    intro();
-    user_move();
-    
-  }
+public class test {
 
-  public static void intro() {
-    System.out.println("Welcom to Maze Runner!");
-    System.out.println("Here is your current position:");
-    myMap.printMap();
-  }
+    Maze myMap = new Maze();
 
-  public static void user_move() {
-    String input = new Scanner();
-    
-    System.out.print("Where would you like to move? (R, L, U, D) ");
-    String move = input.next();
-    if (move != "R" && move != "L" && move != "U" && move != "D") {
-      System.out.println("Oops, wrong input. Try again!");
-      user_move();
-    } else if ( (move == "R" && myMap.canIMoveRight() == false) || (move == "L" && myMap.canIMoveLeft() == false) || (move == "U" && myMap.canIMoveUp() == false) || (move == "D" && myMap.canIMoveDown() == false) ){
-      System.out.println("Sorry, you’ve hit a wall.");
-      user_move();      
-    } else if (move == "R") {
-      myMap.moveRight();
-      myMap.printMap();
-    } else if (move == "L") {
-      myMap.moveLeft();
-      myMap.printMap();
-    } else if (move == "U") {
-      myMap.moveUp();
-      myMap.printMap();
-    } else {
-      myMap.moveDown();
-      myMap.printMap();
+    public static void main(String[] args) {
+
+        intro();
+        user_move();
+
     }
-    
-    if (myMap.didIWin() == true) {
-      System.out.println("Congratulations, you made it out alive!");
-    } else {
-      user_move();
+
+    public static void intro() {
+        System.out.println("Welcome to Maze Runner!");
+        System.out.println("Here is your current position:");
+        myMap.printMap();
     }
-  }
+
+    public static void user_move() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Where would you like to move? (R, L, U, D) ");
+        String move = input.next();
+        if (move != "R" && move != "L" && move != "U" && move != "D") {
+            System.out.println("Oops, wrong input. Try again!");
+            user_move();
+        } else if ( (move == "R" && myMap.canIMoveRight() == false) || (move == "L" && myMap.canIMoveLeft() == false) || (move == "U" && myMap.canIMoveUp() == false) || (move == "D" && myMap.canIMoveDown() == false) ){
+            System.out.println("Sorry, you’ve hit a wall.");
+            user_move();
+        } else if (move == "R") {
+            myMap.moveRight();
+            myMap.printMap();
+        } else if (move == "L") {
+            myMap.moveLeft();
+            myMap.printMap();
+        } else if (move == "U") {
+            myMap.moveUp();
+            myMap.printMap();
+        } else {
+            myMap.moveDown();
+            myMap.printMap();
+        }
+
+        if (myMap.didIWin() == true) {
+            System.out.println("Congratulations, you made it out alive!");
+        } else {
+            user_move();
+        }
+    }
 
 }
