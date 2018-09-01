3ad0ea514280a95d37b25ca2944cf84741fc38a2
daydreamersjp <42426597+daydreamersjp@users.noreply.github.com>
2018-08-31 21:28:42 -0400

    Update MazeRunner_part2.java

--- DEV276x Learn to Program in Java/Final Project/MazeRunner_part2.java
+++ DEV276x Learn to Program in Java/Final Project/MazeRunner_part2.java
@@ -7,18 +7,18 @@ public class MazeRunner_part2 {
     
     int moves_cnt = 0;
     intro();
-    user_move(move_cnt);
+    user_move(moves_cnt);
     
   }
 
   public static void intro() {
-    System.out.println("Welcom to Maze Runner!");
+    System.out.println("Welcome to Maze Runner!");
     System.out.println("Here is your current position:");
     myMap.printMap();
   }
 
   public static void user_move(int moves_cnt) {
-    String input = new Scanner();
+    Scanner input = new Scanner();
     
     System.out.print("Where would you like to move? (R, L, U, D) ");
     String move = input.next();
