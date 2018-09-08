c6a06c004e0bd69a10e619f1c4776672306a515e
daydreamersjp <42426597+daydreamersjp@users.noreply.github.com>
2018-09-07 23:49:57 -0400

    Update CompanyStructure.java

--- DEV277x Object Oriented Programming in Java/Module 3/CompanyStructure.java
+++ DEV277x Object Oriented Programming in Java/Module 3/CompanyStructure.java
@@ -1,30 +1,23 @@
 public class CompanyStructure {
     public static void main(String[] args) {
-        TechnicalLead CTO = new TechnicalLead("Satya Nadella");
-        SoftwareEngineer seA = new SoftwareEngineer("Kasey");
-        SoftwareEngineer seB = new SoftwareEngineer("Breana");
-        SoftwareEngineer seC = new SoftwareEngineer("Eric");
-        CTO.addReport(seA);
-        CTO.addReport(seB);
-        CTO.addReport(seC);
-        System.out.println(CTO.getTeamStatus());
+        TechnicalEmployee te = new TechnicalEmployee("Michale Jackson");
+        BusinessEmployee be = new BusinessEmployee("Total Tenbus");
 
-        TechnicalLead VPofENG = new TechnicalLead("Bill Gates");
-        SoftwareEngineer seD = new SoftwareEngineer("Winter");
-        SoftwareEngineer seE = new SoftwareEngineer("Libby");
-        SoftwareEngineer seF = new SoftwareEngineer("Gizan");
-        SoftwareEngineer seG = new SoftwareEngineer("Zaynah");
-        VPofENG.addReport(seD);
-        VPofENG.addReport(seE);
-        VPofENG.addReport(seF);
-        VPofENG.addReport(seG);
-        System.out.println(VPofENG.getTeamStatus());
+        System.out.println(te.getEmployeeID());
+        System.out.println(be.getEmployeeID());
 
-        BusinessLead CFO = new BusinessLead("Amy Hood");
-        Accountant actA = new Accountant("Niky");
-        Accountant actB = new Accountant("Andrew");
-        CFO.addReport(actA, CTO);
-        CFO.addReport(actB, VPofENG);
-        System.out.println(CFO.getTeamStatus());
+        System.out.println(te.getBaseSalary());
+        System.out.println(be.getBaseSalary());
+
+        te.addSuccessfulCheckins();
+        te.addSuccessfulCheckins();
+
+        be.useBudget(450.50);
+
+        System.out.println(te.employeeStatus());
+        System.out.println(be.employeeStatus());
+
+
     }
+
 }
