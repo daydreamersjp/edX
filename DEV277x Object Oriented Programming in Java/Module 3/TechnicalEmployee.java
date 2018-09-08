70e8e8bef1babdc0b1450c9f5157006643982e3f
daydreamersjp <42426597+daydreamersjp@users.noreply.github.com>
2018-09-07 23:50:35 -0400

    Update TechnicalEmployee.java

--- DEV277x Object Oriented Programming in Java/Module 3/TechnicalEmployee.java
+++ DEV277x Object Oriented Programming in Java/Module 3/TechnicalEmployee.java
@@ -1,22 +1,23 @@
 public class TechnicalEmployee extends Employee {
-    
+
     private static int successfulCheckins = 0;
-    
+
     public TechnicalEmployee(String name) {
-        this.name = name;
-        this.baseSalary = 75000;
-        this.successfulCheckins++;
-        this.employeeID++;
+        super(name, 75000);
     }
-    
+
     public int getSuccessfulCheckins() {
         return this.successfulCheckins;
-    }    
-    
+    }
+
+    public void addSuccessfulCheckins() {
+        this.successfulCheckins++;
+    }
+
     public String employeeStatus() {
         String mt2 = "";
         if (this.getSuccessfulCheckins() > 1) { mt2 = "s"; }
-        System.out.println(this.getEmployeeID() + " " + this.getName() + " has " + this.getSuccessfulCheckins() + " successful check in" + mt2);
+        return this.getEmployeeID() + " " + this.getName() + " has " + this.getSuccessfulCheckins() + " successful check in" + mt2;
     }
 
 }
