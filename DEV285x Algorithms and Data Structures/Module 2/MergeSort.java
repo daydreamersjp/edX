768caa1a5f555aa06185809315f6d141c79c0fb7
daydreamersjp <42426597+daydreamersjp@users.noreply.github.com>
2018-09-10 04:59:39 -0400

    Create MergeSort.java

---
+++ DEV285x Algorithms and Data Structures/Module 2/MergeSort.java
@@ -0,0 +1,22 @@
+public class MergeSort {
+
+    public static void main(String[] args) {
+
+        int[] inputArray1 = {9, 5, -1, 4, 20, -100, 2, 1};
+        int[] inputArray2 = {5, 2, 7, -1, 100};
+        int[] inputArray3 = {12, 4, 8, 1, -10, 12, 1, 2};
+
+        System.out.println(mergeSort(inputArray1));
+
+    }
+
+    public int[] mergeSort(int[] arr) {
+
+        if (arr.length < 2) { return arr; }
+        else {
+            
+        }
+
+    }
+
+}
