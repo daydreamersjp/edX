54a317334064f2fab42123e467590f248a17f31f
daydreamersjp <42426597+daydreamersjp@users.noreply.github.com>
2018-10-16 23:51:32 -0400

    Update Objects.cpp

--- DEV210x Introduction to Cpp/Rectangle/Objects.cpp
+++ DEV210x Introduction to Cpp/Rectangle/Objects.cpp
@@ -5,9 +5,11 @@ int main() {
 	Rectangle aggregate{ 4, 7 };
 	value.resize(5, 5);
 	aggregate.resize(1, 2);
+	Rectangle const immutable{ 5, 10 };
 
 	int area_of_value{ value.area() };
 	int area_of_aggregate{ aggregate.area() };
+	int area_of_immutable{ immutable.area() };
 
 	return 0;
 }
